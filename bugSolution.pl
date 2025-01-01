473845c6e9a41569cc6ff994df2508ad43535b59
BugHunter-X <askzenai@gmail.com>
2025-01-01 11:22:05 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $count = 0;
+
+while (<>) {
+    if (/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+
+/) {
+        $count++;
+    }
+}
+
+print "Number of email addresses found: $count\n";
